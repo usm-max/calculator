a1cda3be8ed9036e6657e3e59c1347846d71c0bf
usm-max <usm7work@gmail.com>
2024-10-08 01:49:52 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,21 +1,21 @@
+import streamlit as st
 from addition import add
 from  subtraction import sub
 from multiplication import mul
 from division import div
-
-first = float(input("Enter first number "))
-second = float(input("Enter second number "))
-print("Enter operator  +  or  /  or  *  or  -   ")
-operator = input()
-
-result=0.0
+st.title('-----Calculator-----')
+first = st.number_input("Enter first number")
+operator = st.text_input("Enter   +  or  -  or  *  or  / ")
+second = st.number_input("Enter second number")
+result1=0.0
 if operator=="+":
-    result=add(first,second)
+    result1=add(first,second)
 elif operator == "-":
-    result=sub(first,second)
+    result1=sub(first,second)
 elif operator == "*":
-    result=mul(first,second)
+    result1=mul(first,second)
 elif operator == "/":
-    result=div(first,second)
+    result1=div(first,second)
+if(st.button('Calculate')):
+    st.success(result1)
 
-print(result)
